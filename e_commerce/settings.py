b9485d9b1718852d7f7c21dd58301cddb40cd76f
charleslima <charlesrobertolima@gmail.com>
2020-06-28 11:13:01 -0300

    update  e_commerce/forms.py , e_commerce/view.html, template/about/login.html, template/auth/register.html, template/contact/view.html

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -98,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
